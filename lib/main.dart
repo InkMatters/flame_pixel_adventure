7f7cc80fa3f5a0da759c89669955dfe16ceb8af8
Héctor Alvarez <halvarezr@outlook.com>
2023-09-29 23:31:18 -0600

    assets refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Flame.device.fullScreen();
+  Flame.device.setLandscape();
+
   runApp(GameWidget(game: FlamePixelAdventure()));
 }
 
